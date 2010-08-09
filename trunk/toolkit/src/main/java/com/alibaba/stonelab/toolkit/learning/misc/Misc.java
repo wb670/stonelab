127533c349e8a4b4e09bf67d55e85a634bed5ee5
stone2083 <stone2083@10ebbd10-f3f5-8434-4617-7576d192ee63>
2010-08-09 09:31:10 +0000

    ibatis下单元测试新思路.

---
+++ trunk/toolkit/src/main/java/com/alibaba/stonelab/toolkit/learning/misc/Misc.java
@@ -0,0 +1,17 @@
+/*
+ * Copyright 1999-2010 Alibaba.com All right reserved. This software is the
+ * confidential and proprietary information of Alibaba.com ("Confidential
+ * Information"). You shall not disclose such Confidential Information and shall
+ * use it only in accordance with the terms of the license agreement you entered
+ * into with Alibaba.com.
+ */
+package com.alibaba.stonelab.toolkit.learning.misc;
+
+/**
+ * @author Stone.J 2010-8-3 下午06:11:31
+ */
+public class Misc {
+
+    public static void main(String[] args) throws Exception {
+    }
+}
