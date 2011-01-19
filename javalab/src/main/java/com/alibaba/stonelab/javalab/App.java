425f391b8577f23819c973389d59bbe49a4ef761
stone2083 <stone2083@10ebbd10-f3f5-8434-4617-7576d192ee63>
2011-01-19 11:54:37 +0000

    add javalab

---
+++ javalab/src/main/java/com/alibaba/stonelab/javalab/App.java
@@ -0,0 +1,13 @@
+package com.alibaba.stonelab.javalab;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
