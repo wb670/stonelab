bef097c5406e04bfe04503dd27f728262554d1ca
stone2083 <stone2083@10ebbd10-f3f5-8434-4617-7576d192ee63>
2012-12-27 09:37:18 +0000

    delete pos

--- trunk/pos/tinix/Tinix/chapter2/win/run.bat
+++
@@ -1,2 +0,0 @@
-cd "d:\Program Files\Bochs-2.1.1\Tinix"
-..\bochs -q -f bochsrc.bxrc
--- trunk/pos/tinix/Tools/ELFParser/StdAfx.cpp
+++
@@ -1,8 +0,0 @@
-// stdafx.cpp : source file that includes just the standard includes
-//	ELFParser.pch will be the pre-compiled header
-//	stdafx.obj will contain the pre-compiled type information
-
-#include "stdafx.h"
-
-
-
