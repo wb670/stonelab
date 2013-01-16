07bcaa540b59ce30b41618729955264783eb8075
stone2083 <stone2083@10ebbd10-f3f5-8434-4617-7576d192ee63>
2013-01-16 02:53:21 +0000

    raspctl V0.1.1 release

--- hellowrold/servlet/WebContent/example/version.jsp
+++
@@ -1,3 +0,0 @@
-<%@ page language="java" contentType="text/html; charset=GBK"
-	pageEncoding="GBK"%>
-Version:<%=application.getAttribute("__VERSION__")%>
--- scripts/autodeploy/bin/deploy.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-war=$1
-#TARGET='/home/admin/web-deploy/tomcat/webapps/ROOT'
-TARGET='/home/stone/tmp/tester/'
-rm -fr $TARGET
-unzip $war -d $TARGET
