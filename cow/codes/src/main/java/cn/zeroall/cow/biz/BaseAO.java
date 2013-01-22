db2afc37ed41551d71b67147bf736b2c6a0e567c
stone2083 <stone2083@10ebbd10-f3f5-8434-4617-7576d192ee63>
2013-01-22 06:40:43 +0000

    raspctl v0.1.2 released

--- cow/codes/src/main/java/cn/zeroall/cow/biz/BaseAO.java
+++
@@ -1,9 +0,0 @@
-package cn.zeroall.cow.biz;
-
-/**
- * 
- * @author <a href="mailto:stone2083@yahoo.cn">Maurice.J</a> Feb 15, 2009
- */
-public interface BaseAO {
-
-}
--- scripts/misc/staff.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-cert_base="/Users/stone/Documents/私人"
-cert="$cert_base/liwang.pem"
-if [ "$1" == "" ];then
-    echo 'staff.sh name'
-    exit
-fi
-curl -sE $cert -k "https://www.cn.alibaba-inc.com/search.php?key=$1" | grep -i 'td\d_' | gsed 's/<[^>]*>/ /g' | gsed 's/com/com\n/g' | gsed 's/工号.*邮箱地址//g' | gsed 's/\s\+/  /g' | gsed 's/^\s\+//g' | gsed 's/&nbsp//g'
