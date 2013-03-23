a0b8a3b532335b33dea4845d9b7e6671d84f1f64
stone2083 <stone2083@10ebbd10-f3f5-8434-4617-7576d192ee63>
2013-03-23 07:08:16 +0000

    pdbx v0.2.0 released

---
+++ tags/pdbx-0.2.0/pdbx/__init__.py
@@ -0,0 +1,1 @@
+from pdbx import Rpdb
---
+++ tags/pdbx-0.2.0/setup.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+
+from distutils.core import setup
+
+setup(
+    name="pdbx",
+    version="0.2.0",
+    description="Remote PDB Debugger Based On PDB",
+    author="Stone.J",
+    author_email="stone2083@yahoo.cn",
+    url="https://code.google.com/p/stonelab/wiki/pdbx",
+    packages=["pdbx"],
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: ISC License (ISCL)",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+        "Operating System :: POSIX",
+        "Programming Language :: Python :: 2.5",
+        "Programming Language :: Python :: 2.6",
+        "Programming Language :: Python :: 2.7",
+        "Topic :: Software Development :: Debuggers",
+    ]
+)
