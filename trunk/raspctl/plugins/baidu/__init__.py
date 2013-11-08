445ca8d5d16d6278c58941c25cb2f92285f8de99
stone2083 <stone2083@10ebbd10-f3f5-8434-4617-7576d192ee63>
2013-11-08 02:29:48 +0000

    baidu

--- trunk/raspctl/plugins/baidu/__init__.py
+++ trunk/raspctl/plugins/baidu/__init__.py
@@ -3,47 +3,38 @@
 from rasplib import Plugin
 from urllib2 import urlopen, Request, quote
 import re, json
-from pyquery import PyQuery
 
 urls = (
     '/', 'Index',
 )
 
-plugin = Plugin('baidu','stone2083', '0.1', urls)
+plugin = Plugin('baidu','stone2083', '0.2', urls)
 
 
 class Index:
 
-    ENCODING    = 'GBK'
-    SEARCH_URL  = 'http://video.baidu.com/v?word=%s'
+    ENCODING    = 'utf-8'
+    SEARCH_URL  = 'http://app.video.baidu.com/app?ct=905969664&word=%s&ie=utf-8&version=4.4.1'
+    RES_URL     = 'http://app.video.baidu.com/xqsingle/?worktype=adnativetvplay&id=%s&site=%s&version=4.4.1'
     HEADERS     = {
         'User-Agent':'Mozilla/5.0 (Windows NT 6.2; rv:16.0) Gecko/20100101 Firefox/16.0'
     }
 
     def GET(self):
-        kw = u'少年包青天'
-        kw = quote(kw.encode(Index.ENCODING))
-        info = urlopen(Request(Index.SEARCH_URL % (kw), headers=Index.HEADERS)).read().decode(Index.ENCODING)
-        info = info.replace('/browse_static', 'http://video.baidu.com/browse_static')
-        pq = PyQuery(info)
-        #pq('head>title').remove()
-        #pq('div.poster-sec').remove()
-        #pq('div#header').remove()
-        #pq('div#navbar').remove()
-        #pq('div#normalarea').remove()
-        header, body = pq('head').html(), pq('body').html()
-        return plugin.render.index2(
-                header = header, body = body, info=info, pq = pq)
+        return plugin.render.index()
 
 
     @classmethod
     def search(cls, kw):
         kw = quote(kw.encode(Index.ENCODING))
-        info = urlopen(Request(Index.SEARCH_URL % (kw), headers=Index.HEADERS)).read().decode(Index.ENCODING)
-        info = info.replace('/browse_static', 'http://video.baidu.com/browse_static')
-        headers = re.findall('<head>([^真]*)</head>', info)[0]
-        print headers
-        
+        info  = urlopen(Request(Index.SEARCH_URL % (kw), headers=Index.HEADERS)).read().decode(Index.ENCODING)
+        return json.loads(info) 
+
+    @classmethod
+    def res(cls, id, site):
+        return json.loads(
+                urlopen(Request(Index.RES_URL % (id, site), headers=Index.HEADERS)).read().decode(Index.ENCODING))
+
 
     @classmethod
     def play(cls, name, url):
